56f22d26f6dc83e2c151421bcb3423e7ae2de3d9
Brandon Navra <brandon.navra@gmail.com>
2018-02-21 00:01:22 +1100

    Ensure we use PostgreSQL JSONField

--- src/auditlog/migrations/0005_logentry_additional_data_verbose_name.py
+++ src/auditlog/migrations/0005_logentry_additional_data_verbose_name.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.postgres.fields.jsonb import JSONField
 from django.db import migrations, models
-import jsonfield.fields
 
 
 class Migration(migrations.Migration):
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='logentry',
             name='additional_data',
-            field=jsonfield.fields.JSONField(null=True, verbose_name='additional data', blank=True),
+            field=JSONField(null=True, verbose_name='additional data', blank=True),
         ),
     ]
