d7beda43769ff87233c59318c846bdca43680440
Brandon Navra <brandon.navra@gmail.com>
2018-02-28 07:27:18 +1100

    Add index to timestamp

--- src/auditlog/migrations/0001_initial.py
+++ src/auditlog/migrations/0001_initial.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
                 ('object_repr', models.TextField(verbose_name='object representation')),
                 ('action', models.PositiveSmallIntegerField(verbose_name='action', choices=[(0, 'create'), (1, 'update'), (2, 'delete')])),
                 ('changes', models.TextField(verbose_name='change message', blank=True)),
-                ('timestamp', models.DateTimeField(auto_now_add=True, verbose_name='timestamp')),
+                ('timestamp', models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='timestamp')),
                 ('actor', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, verbose_name='actor', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
                 ('content_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='+', verbose_name='content type', to='contenttypes.ContentType')),
             ],
